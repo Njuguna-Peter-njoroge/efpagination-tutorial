ff186a5117dcdcb4966d2065d8fe9618fceeff08
Peter Njoroge <njugunahpeternjoroge@gmail.com>
2026-09-12 19:33:56 +0300

    :commit

---
+++ EfCoreTutorialDotNet6/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfCoreTutorialDotNet6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context)
+        {
+           _context = context;
+        }
+
+        [HttpGet("{page}")]
+
+        public async Task<ActionResult<List<Product>>> GetProducts(int page)
+        {
+
+            if (_context.Products == null)
+            
+                return NotFound();
+
+            var pageResults = 3f;
+
+            var pageCount = Math.Ceiling(_context.Products.Count() / pageResults);
+            
+
+            var products = await _context.Products
+                .Skip(page -1 * (int)pageResults)
+                .Take((int)pageResults)
+                .ToListAsync();
+                 
+
+
+            return Ok(products);
+        }
+
+    }
+}
